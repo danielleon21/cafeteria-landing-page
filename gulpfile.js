91ae7b026e40ac9c8db0aac9a950f506e9b432e1
Daniel León <matusleonc@outlook.com>
2022-09-06 13:59:35 -0500

    created task to compile sass to css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function task(done) {
-  console.log("From my first task...");
+// function task(done) {
+//   console.log("From my first task...");
+//   done();
+// }
+
+// // You have to name your task on exports functions and thats the name you will call them from terminal
+// exports.firstTask = task;
+
+// importing gulp
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function css(done) {
+  // compile sass
+  // 1. Identify the file
+  // 2. Compile
+  // save the .css file
+  src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
   done();
 }
 
-exports.firstTask = task;
+exports.css = css;
